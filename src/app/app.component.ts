3149c4456743ffe22c624e733f33012439a648c6
erickortiz9814 <erickortiz9814@gmail.com>
2024-03-15 13:00:27 -0600

    Correcion boton agregar y boton eliminar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { WeatherComponent } from './weather/weather.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  componentRefs: any[] = [];
   @ViewChild('weatherContainer', { read: ViewContainerRef }) weatherContainer!: ViewContainerRef;
 
   constructor(private componentFactoryResolver: ComponentFactoryResolver) {}
@@ -14,7 +15,17 @@ export class AppComponent {
   addComponent(): void {
     const componentFactory = this.componentFactoryResolver.resolveComponentFactory(WeatherComponent);
     const componentRef = this.weatherContainer.createComponent(componentFactory);
+    this.weatherContainer.element.nativeElement.appendChild(componentRef.location.nativeElement);
   }
+
+  deleteComponent(): void {
+    const componentRef = this.weatherContainer.get(this.weatherContainer.length - 1);
+    
+    if (componentRef) {
+      componentRef.destroy();
+    }
+  }
+
   title = 'WeatherAppAngular17';
 }
 
